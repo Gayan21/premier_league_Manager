3be5aaa16dd70c0a2bb64617c271eaa9edae6478
Gayan21 <68651319+Gayan21@users.noreply.github.com>
2023-06-16 18:51:15 +0530

    added navigation bar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, NavLink, Route, Switch } from 'react-router-dom';
 
-function App() {
+// Home component
+const Home = () => <h2>Welcome to the Home Page</h2>;
+
+// Point Table component
+const PointTable = () => <h2>Check out the Point Table</h2>;
+
+// Played Matches component
+const PlayedMatches = () => <h2>View Played Matches</h2>;
+
+// Add Match component
+const AddMatch = () => <h2>Add a New Match</h2>;
+
+// NavigationBar component
+const NavigationBar = () => {
   return (
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <NavLink exact to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/point-table">Point Table</NavLink>
+          </li>
+          <li>
+            <NavLink to="/played-matches">Played Matches</NavLink>
+          </li>
+          <li>
+            <NavLink to="/add-match">Add Match</NavLink>
+          </li>
+        </ul>
+      </nav>
+
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/point-table" component={PointTable} />
+        <Route path="/played-matches" component={PlayedMatches} />
+        <Route path="/add-match" component={AddMatch} />
+      </Switch>
+    </Router>
+  );
+};
+
+// App component
+const App = () => {
+  return (
+    <div>
+      <NavigationBar />
     </div>
   );
-}
+};
 
 export default App;
+
