cabc8d4be62da14fe0bcd969f877db3ae07a3347
Gayan21 <68651319+Gayan21@users.noreply.github.com>
2023-06-16 19:50:05 +0530

    added nav bar

--- src/App.js
+++ src/App.js
@@ -1,55 +1,43 @@
+
 import React from 'react';
-import { BrowserRouter as Router, NavLink, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+import './App.css';
+import PremierLeagueLogo from './Premier_League.png';
 
-// Home component
-const Home = () => <h2>Welcome to the Home Page</h2>;
-
-// Point Table component
-const PointTable = () => <h2>Check out the Point Table</h2>;
-
-// Played Matches component
-const PlayedMatches = () => <h2>View Played Matches</h2>;
-
-// Add Match component
-const AddMatch = () => <h2>Add a New Match</h2>;
-
-// NavigationBar component
-const NavigationBar = () => {
-  return (
-    <Router>
-      <nav>
-        <ul>
-          <li>
-            <NavLink exact to="/">Home</NavLink>
-          </li>
-          <li>
-            <NavLink to="/point-table">Point Table</NavLink>
-          </li>
-          <li>
-            <NavLink to="/played-matches">Played Matches</NavLink>
-          </li>
-          <li>
-            <NavLink to="/add-match">Add Match</NavLink>
-          </li>
-        </ul>
-      </nav>
-
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/point-table" component={PointTable} />
-        <Route path="/played-matches" component={PlayedMatches} />
-        <Route path="/add-match" component={AddMatch} />
-      </Switch>
-    </Router>
-  );
-};
-
-// App component
 const App = () => {
   return (
-    <div>
-      <NavigationBar />
-    </div>
+    <Router>
+      <div>
+        <header className="header">
+          <div className="navbar-logo">
+            <a href="/">
+              <img src={PremierLeagueLogo} alt="Premier League" />
+            </a>
+          </div>
+          <nav className="navbar-container">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <NavLink exact to="/" className="nav-link" activeClassName="active">Home</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="/point-table" className="nav-link" activeClassName="active">Point Table</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="/played-matches" className="nav-link" activeClassName="active">Played Matches</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="/add-match" className="nav-link" activeClassName="active">Add Match</NavLink>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        {/* Content section */}
+        <div className="app">
+          <h2>Welcome to the Premier League</h2>
+          {/* Add your content here */}
+        </div>
+      </div>
+    </Router>
   );
 };
 
