659856e38adeca8ac24e83c0fac95f32658b67ba
Gayan21 <68651319+Gayan21@users.noreply.github.com>
2023-06-18 22:41:29 +0530

    added cards and description

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import addLogo from './add.png';
+import clubsLogo from './clubs.png';
+import pointsLogo from './points.png';
+import viewLogo from './View.png';
 
 const Home = () => {
   return (
@@ -6,24 +10,29 @@ const Home = () => {
       <h2>Welcome to the Premier League</h2>
       <div className="card-container">
         <div className="card">
-          <img src="image1.jpg" alt="Image 1" />
-          <h3>Card 1</h3>
-          <p>Card 1 description</p>
+          <img src={addLogo} alt="add" />
+          <h3>Points Table</h3>
+          <p>In Premier League Manager you can see statistics of every club that
+            is in premier league and you can sort the clubs acording to Points ,
+            Goals and number of Wins.</p>
         </div>
         <div className="card">
-          <img src="image2.jpg" alt="Image 2" />
-          <h3>Card 2</h3>
-          <p>Card 2 description</p>
+          <img src={clubsLogo} alt="clubs" />
+          <h3>Add Match</h3>
+          <p> By using Premier League Manager u can add a match between two
+            randomly selected teams. the match infomation will also be updated
+            randomly.</p>
         </div>
         <div className="card">
-          <img src="image3.jpg" alt="Image 3" />
-          <h3>Card 3</h3>
-          <p>Card 3 description</p>
+          <img src={pointsLogo} alt="points" />
+          <h3>View Matches</h3>
+          <p> You can view all the matches played in the league and they are
+            sorted by the date.</p>
         </div>
         <div className="card">
-          <img src="image4.jpg" alt="Image 4" />
-          <h3>Card 4</h3>
-          <p>Card 4 description</p>
+          <img src={viewLogo} alt="view" />
+          <h3>View Clubs</h3>
+          <p>You can view all the clubs that currenty in the premier league.</p>
         </div>
       </div>
     </div>
